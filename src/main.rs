ad9fbc98a0d72229afd62f6f6ba5d5764c55412e
Yue Jiao <cleamoon@hotmail.com>
2022-01-14 00:14:46 +0100

    Add basic support for double fault exceptions

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,17 @@ pub extern "C" fn _start() -> ! {
     rosystarling::init();
 
     // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    //x86_64::instructions::interrupts::int3();
+
+    // trigger a page fault
+    //unsafe {
+    //    *(0xdeadbeef as *mut u64) = 42;
+    //};
+
+    //fn stack_overflow() {
+    //    stack_overflow();
+    //}
+    //stack_overflow();
 
     #[cfg(test)]
     test_main();
